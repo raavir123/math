a8b42ad8c891c3c8ca9bbfc6b6325b7121f5f101
ratnakar <ratnakarbabu.raavi@gmail.com>
2026-03-09 15:08:26 +0530

    third version of files

--- calculator.py
+++ calculator.py
@@ -1,11 +1,30 @@
-# use official python image
-FROM python:3.9
+print("Simple Calculator")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiplication")
+print("4. Divison")
 
-# set working directory
-WORKDIR /app
+choice = input("Enter choice (1/2/3/4): ")
 
-# code copy
-COPY calculator.py .
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
-# command to run app
-CMD ["python", calculator.py"]
+if choice == '1':
+    print("Result:", num1 + num2)
+
+ elif choice == '2':
+    print("Result:", num1 - num2)
+
+ elif choice == '3':
+    print("Result:", num1 * num2)
+
+ elif choice =='4':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error! division by zero.")
+
+else:
+    print("Invalid option")        
+
+    
